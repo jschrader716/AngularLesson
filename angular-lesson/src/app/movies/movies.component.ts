b0fcaa88f7c87924a000bd83d683b1e7b9c6873c
Brooke Daniels <31452787+bed3344@users.noreply.github.com>
2019-11-03 12:22:06 -0500

    Added demo for milestone 2 topics

---
+++ angular-lesson/src/app/movies/movies.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Movie } from '../movie';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.css']
+})
+export class MoviesComponent implements OnInit {
+  myMovie: Movie = {
+    name: 'Jaws',
+    rating: 8
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
